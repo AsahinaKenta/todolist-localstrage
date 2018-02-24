61747e5075f8258541854843c5a670732b8c5b00
AsahinaKenta <0eb0r1517875j0a@gmail.com>
2018-02-24 13:18:19 +0900

    一時保存

---
+++ script.js
@@ -0,0 +1,45 @@
+// ローカルストレージへの保存
+// localStorage.setItem('key','value');
+// keyとvalueのペアで保存する
+
+// localStorage.setItem('name','朝比奈');
+//
+// var name = '石黒';
+// localStorage.setItem('名前',name);
+//
+// var num = 100;
+// localStorage.setItem('num',num);
+//
+// var fruits = ['りんご','みかん','バナナ'];
+// localStorage.setItem('果物',fruits);
+
+// JSON
+// JavaScript Object Notation
+
+// JSON形式に変換
+// var fruitsJson = JSON.stringify(fruits);
+// localStorage.setItem('果物',fruitsJson);
+
+// ローカルストレージかデータを取得
+//　localStorage.getItem('key');
+
+console.log(localStorage.getItem('name'));
+
+var getData = localStorage.getItem('名前');
+console.log(getData);
+
+var getNum = localStorage.getItem('num');
+console.log(getNum);
+
+console.log( +localStorage.getItem('num'));
+
+var getFruits = localStorage.getItem('果物');
+console.log(getFruits);
+var jsonParse = JSON.parse(getFruits);
+console.log(jsonParse);
+
+// データの削除
+// localStorage.removeKey('Key');
+localStorage.removeItem('名前');
+
+localStorage.clear();
